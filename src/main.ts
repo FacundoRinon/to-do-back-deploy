67f9f78339c419530aa2100cc5372aaacda133d6
FacundoRinon <facuzionff@gmail.com>
2024-07-02 18:47:26 -0300

    regular config

--- src/main.ts
+++ src/main.ts
@@ -9,14 +9,14 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   // Configuración global de CORS para permitir todas las solicitudes
-  // app.enableCors();
+  app.enableCors();
 
   // Opcional: Configuración específica para un origen y métodos específicos
-  app.enableCors({
-    origin: 'http://localhost:5173', // Reemplaza esto con tu URL de desarrollo
-    methods: ['GET', 'POST'], // Métodos HTTP permitidos
-    allowedHeaders: ['Content-Type'], // Encabezados permitidos
-  });
+  // app.enableCors({
+  //   origin: 'http://localhost:5173', // Reemplaza esto con tu URL de desarrollo
+  //   methods: ['GET', 'POST'], // Métodos HTTP permitidos
+  //   allowedHeaders: ['Content-Type'], // Encabezados permitidos
+  // });
 
   app.useGlobalPipes(new ValidationPipe());
 

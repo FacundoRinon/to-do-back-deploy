1d50b021f34b42589d6a36c355f2d8c67098714e
FacundoRinon <facuzionff@gmail.com>
2024-07-02 18:43:00 -0300

    change 3

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,15 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import * as dotenv from 'dotenv';
-import * as cors from 'cors';
 
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
+  // Configuración global de CORS para permitir todas las solicitudes
   app.enableCors();
 
+  // Opcional: Configuración específica para un origen y métodos específicos
   app.enableCors({
     origin: 'http://localhost:5173', // Reemplaza esto con tu URL de desarrollo
     methods: ['GET', 'POST'], // Métodos HTTP permitidos

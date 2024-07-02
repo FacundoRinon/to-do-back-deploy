3ea3ca57ebd0874d29d9ac246ba755114e6503dd
FacundoRinon <facuzionff@gmail.com>
2024-07-02 18:32:08 -0300

    cors configuration

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import * as dotenv from 'dotenv';
 import * as cors from 'cors';
-import { join } from 'path';
 
 async function bootstrap() {
   dotenv.config();
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
+  app.enableCors();
   // app.use(
   //   cors({
   //     origin: 'http://localhost:5173',
@@ -15,13 +17,15 @@ async function bootstrap() {
   //   }),
   // );
 
-  const corsOptions = {
-    origin: 'http://localhost:5173', // El dominio permitido para las solicitudes CORS
-    methods: ['GET', 'POST'], // Métodos HTTP permitidos
-    // allowedHeaders: ['Content-Type'],
-  };
+  // const corsOptions = {
+  //   origin: 'http://localhost:5173', // El dominio permitido para las solicitudes CORS
+  //   methods: ['GET', 'POST'], // Métodos HTTP permitidos
+  // allowedHeaders: ['Content-Type'],
+  // };
 
-  app.use(cors(corsOptions));
+  // app.use(cors(corsOptions));
+
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(3000);
 }
